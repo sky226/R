28d2430afc09baea151dd1e3627228761238b6df
sky226 <from2u@ajou.ac.kr>
2020-12-04 22:58:29 +0900

    risk, benefit

--- analysis_1118.R
+++ analysis_1118.R
@@ -22,14 +22,11 @@ library(moonBook)
 library(incidence)
 
 #data import 
-cci<- read.csv("C:/Users/JohnKim_PC/OneDrive/문서/R/CCI_1118.csv")
-data<-read.csv("C:/Users/JohnKim_PC/OneDrive/문서/R/groups_1118.csv")
-data<-data [,c("PERSON_ID","index_date","DEATH","sex","age_group","ros","end_date","QT","QT_SCORE","over75")]
-
+cci<- read.csv("/home/yoonlab6/googleDrive/CCI_1204.csv")
+data<-read.csv("/home/yoonlab6/googleDrive/group_1204.csv")
+data[,c("KEY_SEQ","drop_date","death_date")] <-NULL
 str(data)
 str(cci)
-head(cci)
-head(data)
 
 C <- data.frame (id= cci$PERSON_ID, code = cci$SICK, stringAsFactors= FALSE)
 print(head(C, n=15), row.names =FALSE )
@@ -47,7 +44,7 @@ str(elixhauser)
 #ischemic, hemorrhage stroke
 isc <-c("I63","G45")
 hem <-c("I60","I61","I62")
-stroke<-cci %>% mutate( isc= ifelse(SICK ==isc, 1,0)) %>% mutate(hem = ifelse(SICK==hem,1,0)) 
+stroke <- cci %>% mutate( isc= ifelse(SICK == isc, 1,0)) %>% mutate(hem = ifelse(SICK==hem,1,0)) 
 isc<- aggregate(stroke$isc, list(stroke$PERSON_ID), sum) %>% rename(c("PERSON_ID"="Group.1","isc"="x"))
 hem<- aggregate(stroke$hem, list(stroke$PERSON_ID), sum)%>% rename(c("PERSON_ID"="Group.1","hem"="x"))
 str(hem)
@@ -55,160 +52,144 @@ data <- left_join(data, charlson, by = c("PERSON_ID"="id"))
 data <- left_join(data, elixhauser, by = c("PERSON_ID"="id"))
 data <- left_join(data, hem, by="PERSON_ID")
 data <- left_join(data, isc, by="PERSON_ID")
-
-data[, "x"]<-NULL
 str(data)
 remove(list=c("isc","hem","charlson","elixhauser","C","cci"))
 gc()
 
 ## 층화: risk, benefit  
-data<-data %>% mutate( risk = carit + fed , benefit= ami + valv + cevd + chf + isc + hem) %>% rename(c("is_case" = "ros", "is_event"="DEATH"))
+data<-data %>% mutate( risk = carit + fed , benefit= ami + valv + cevd + chf + isc) %>% rename(c("is_case" = "ros", "is_event"="death"))
 
-head(data)
 str(data)
 
 data[is.na(data)] <- 0
-nrow(data[data$is_case==1,]) #5333
-nrow(data[data$is_case==0,]) # 24723
+nrow(data[data$is_case==1,]) #5492(1~9)
+nrow(data[data$is_case==0,]) # 25754
 
 ##ps matching 
 
 with(data,tapply(is_case,is_event, mean)) #case별로 평균 사망은?
-
-matched <- matchit(is_case ~ sex + age_group + QT + QT_SCORE + ami + rend + cevd + chf+ msld + score + carit +valv + htn + dia+ fed + hypothy + drug + alcohol +aids ,  method="nearest", data=data, ratio=1)
+str(data)
+matched <- matchit(is_case ~ sex + age_group + QT + QT_SCORE + ami + rend + cevd + chf+ msld + score + carit +valv + htn + dia+ fed + hypothy + drug.y + alcohol +aids + hem + isc,  method="nearest", data=data, ratio=1)
 
 case <- match.data(matched, group = 'treat', subclass = "subclass")
-head(case)
 control <- match.data(matched, group = 'control', subclass = "subclass")
-str(case$subclass)
-a <- data.frame(matched$match.matrix)
+a <- data.frame(matched$match.matrix) # CASE, CONTROL ROWNUMBER?
 case$subclass <- rownames(case) #점수별로 
-control <- control[a[!is.na(a)],]
+control <- control[a[!is.na(a)],] # ALL ID IN CONTROL 
 control$subclass <- which(!is.na(a))
 
 total <- rbind(case,control) %>%
-  mutate(index_date = as.Date(index_date, format="%Y-%m-%d"), censored_day = as.Date(end_date, format="%Y-%m-%d")) %>%
-  mutate(duration = censored_day - index_date) %>%
-
+  mutate(index_date = as.Date(index_date, format="%Y-%m-%d"), censored_day = as.Date(end_date, format="%Y-%m-%d")) %>%   mutate(duration = censored_day - index_date) %>% mutate(duration_year = round(duration/365,1))
+str(case)
 str(total)
 total[,c("end_date")] <- NULL
 
 #demographic
 ##n수
-nrow(total[total$is_case == 1,]) #5333
-nrow(total[total$is_case == 0,]) #5333
-nrow(total[total$is_event == 1,]) #48
-nrow(total[total$is_event == 0,]) #10618
+nrow(total[total$is_case == 1,]) #5492
+nrow(total[total$is_case == 0,]) #5492
+nrow(total[total$is_event == 1,]) #59
+nrow(total[total$is_event == 0,]) #10925
 
 ##category
-#unique값이 3이하인것만 범주형 변수로
-str(data)
-l1<-colnames(total)
-l2<- l1[c(1,2,7,14,23,24,25,26,27,28)]
-l1 <- l1[-c(1,2,7,14,23,24,25,26,27,28)]
-
-total <- total %>% mutate_each_(funs(factor), l1) %>% mutate_each_(funs(as.numeric),l2)
+#unique값이 5이하인것만 범주형 변수로
+length(unique(total$benefit))
+select=sapply(total, function(x) length(unique(x)) <=5)
+total[,select] <-lapply(total[, select], factor)
 str(total)
-var<-l1[-4]
+
+var<-rownames(data.frame(select[select ==1]))[c(-1, -23)]
 group_var = "is_case"
 categorical_table = cat_table(var, group_var, total)
 categorical_table$demo_table 
 
 ## numeric 
-var = "QT"
+var = c("QT","age_group","score")
 group_var = "is_case"
 numeric_table = cont_table(var, group_var, total)
 numeric_table$demo_table 
 
 # ALL
-l1[19]="QT"
-all_var = l1[-4]
+
+all_var = rownames(data.frame(select))[c(6:29)]
 group_var = "is_case"
 all_table = demo_table(all_var, group_var, total)
 all_table$demo_table
 all_table$smd_table
 
-mydocx(all_table$demo_table, "C:/Users/JohnKim_PC/OneDrive/문서/R/demographic_table3.csv")
+mydocx(all_table$demo_table, "/home/yoonlab6/googleDrive/table_1205.csv")
 
 ## incidence rate 
 ros<-total[total$is_case==1,]
 ato<-total[total$is_case==0,]
-mm.deaths<-sum (ros$is_event==1)#48-30(ros), 18(ator)
-per.time<-sum((ros$duration)/365)#26015.75-> 12104.46(ros), 13911.29(ato)
+mm.deaths<-sum (ros$is_event==1)#33(ros)
+per.time<-as.integer(sum((ros$duration_year)))#15336
 mortality.rate<-mm.deaths/per.time
-round(100*mortality.rate,1) # 0.2  0.2case per 100py(ros), 0.1(ato) 
+round(100*mortality.rate,1) # 0.2  
 
+mm.deaths2<-sum (ato$is_event==1)#26 (ator)
+per.time2<-as.integer(sum((ato$duration_year)))#18719
+mortality.rate2<-mm.deaths2/per.time2
+round(100*mortality.rate2,1) # 0.1
+remove(list=c("ato","case","control","matched","ros","a","depressiondata2","derpressiondata", "surv.p"))
+gc()
 #survival packages
 str(total)
-total<-total %>% mutate(duration_year = duration/365)
 survival.object <-Surv(total$duration_year, total$is_event ==1)
-
+?Surv
 ##kaplan-meier
 KM.object<-survfit(survival.object ~total$is_case)
-summary(KM.object)
-plot(KM.object, data=total,  fun="cumhaz",mark.time=F, ylab="cumulative hazard", xlab="year",col=c("black","red"))
-legend("bottomright",legend = c('atorvastatin', 'rosuvastatin'),col=1:2, lty=1:2)
-
-ggsurvplot(KM.object, data = total, pval = TRUE, fun='cumhaz', legend.labs = c('control', 'treated'))
-
-#compare 2 groups
-KM.object.ros<-survfit(survival.object ~ total$is_case)
-plot(KM.object.ros, conf.int=T, col=c("black","red"))
-legend("bottomright", pch=1, col=c("black","red"), legend=c("ros", "ator"))
-
+KM.object
 ##log-rank like chi square to compare two curves
 ##(Σ(Oij − E ij)^2 / var(Σ(Oij − E ij)))
-survdiff(survival.object ~ total$is_case)
+surv.p <- survdiff(survival.object ~ total$is_case)
+surv.p
+
+plot(KM.object, fun= function(x){1-x},mark.time=T, ylab="Cumulative death per 1 million prescription", xlab="year",col=c("black","red"), yaxt="n")
+lines(KM.object, col=1:2, lty=2:3)
+my.axis <-axTicks(2)*1000000
+axis(side=2, at= axTicks(2), labels= my.axis)
+text(1, 0.013, paste("p-value:", round(1-pchisq(surv.p$chisq,1),3)))
+legend("bottomright",legend = c('atorvastatin', 'rosuvastatin'),col=1:2, lty=2:3)
+
 ##lml plot
-plot(KM.object.ros, col=c("black","red"), fun="cloglog")
+plot(KM.object, col=c("black","red"), fun="cloglog")
 
 ##proportional hazard
-## variance -> as.numeric
-l1<-colnames(total)
-l1<- l1[c(3,5,6,14,20,21,22)]
-total_cox <- total %>% mutate_each_(funs(as.numeric), l1)
 #cox-로수바스타틴만의 HR
-out1=coxph(Surv(duration_year, is_event==1)~is_case, data=total_cox)
-out1
-##COX-다른 변수들도 포함. 
-out2= coxph(TS ~.-is_event-PERSON_ID-index_date-distance-subclass-censored_day-duration-duration_year, data = total_cox)
-final=step(out2,direction="backward")
-summary(final)
-HRplot(final,type=3, show.CI=T,main="final model selected by backward elimination")
-
-#COX-간략히 
-cox.object<- coxph(Surv(duration_year, is_event==1) ~ is_case + sex +  QT  + over75 + risk_qt + benefit + chronic, data = total_cox )
-summary(cox.object)
-str(total_cox)
-HRplot(cox.object, show.CI = T, main="Cox model with all variables")
-
-
-#층화 stratify-sex
-fit<- coxph(Surv(duration_year, is_event==1) ~ is_case + strata(sex), data = total_cox )
-summary(fit)
-fit2<- coxph(Surv(duration_year, is_event==1) ~ is_case + strata(chronic), data = total_cox )
-summary(fit2)
-## Seperate models for female and male , over 75, risk_qt, beneift, chronic
-
-## benefirc, chronic ; factor-> numeric
-total$benefit1<- cut(total$benefit, breaks=c(1,3, 6),
-                                      include.lowest=T,
-                                      right=F, labels=c("medium","high"))
-total$chronic1<- cut(total$chronic, breaks=c(1,3, 7),
-                     include.lowest=T,
-                     right=F, labels=c("medium","high"))
-fit_<-lapply(split(total, total$chronic1 ), 
-                FUN=function(total){
-                  coxph(Surv(duration_year,is_event==1) ~is_case ,total)
-                  })
 str(total)
+total1=na.omit(total)
+#length(unique(table$col)) >= 2
+total1$TS=Surv(total1$duration_year, total1$is_event ==1)
+out <- coxph(TS ~ is_case + sex +  QT+ QT_SCORE+ ami + rend + cevd + chf+ msld + carit+ valv + fed + hypothy + drug.y + alcohol+  htn + dia + hem + isc , data = total1)
+summary(out)
+HRplot(out, show.CI=T, main="cox model with all variables")
 
-##moonbook
-library(moonBook)
-total_cox$TS=Surv(total_cox$duration_year, total_cox$is_event ==1)
-out= mycph(TS ~.-is_event-PERSON_ID-index_date-distance-subclass-censored_day-duration-duration_year-QT_SCORE-age_group, data = total_cox)
-out
-HRplot(out, type=2, show.CI=TRUE,  main="Hazard ratios of all individual varaibles")
-HRplot(out, type=3, show.CI=TRUE, sig.level=0.05, main="Hazard ratios of significant varaibles")
-str(total_cox)
-str(total)
+##mycph
+require(moonBook)
+out2<- mycph(TS ~ is_case + sex +  QT+ QT_SCORE+ ami + rend + cevd + chf+  carit+ valv + fed + hypothy +  alcohol+  htn + dia  , data = total1)
+HRplot(out2, type=2, show.CI =T, main="Hazard ratios of all individual variables")
+
+#data_cleansing
+str(data)
+unique(data$risk)
+
+# RISK Group Hr
+plot(total1$risk)
+total2<-total1[(total1$risk==1)| (total1$risk==2),]
+out3= coxph(Surv(total2$duration_year, total2$is_event ==1)~ is_case + sex +  QT+ QT_SCORE+ ami + rend + cevd + chf+  carit+ valv + fed + hypothy +  alcohol+  htn + dia  , data = total2)
+summary(out3)
+HRplot(out3, show.CI=T, main="cox model with all variables_risk group")
+
+## benefir group hr 
+plot(total1$benefit)
+total3<-total1[total1$benefit==c(1,2,3,4),]
+out4= coxph(Surv(total3$duration_year, total3$is_event ==1)~ is_case + sex +  QT+ QT_SCORE+ ami + rend + cevd + chf+  carit+ valv + fed + hypothy +  alcohol+  htn + dia  , data = total3)
+summary(out4)
+HRplot(out4, show.CI=T, main="cox model with all variables_benefit group")
+
+
+
+
+
+
